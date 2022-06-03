6197c35b6428356255cc406e0e3843c271783909
nnhjs <nnhungjs@gmail.com>
2022-06-03 14:00:59 +0700

    useState, useRef, useEffect

--- react.js
+++ react.js
@@ -1,6 +1,7 @@
 const React = (function() {
   const hooks = [];
   let idx = 0;
+
   function useState(initVal) {    
     let _idx = idx;
     const state =  hooks[_idx] || initVal;
@@ -9,6 +10,24 @@ const React = (function() {
     }
     idx++;
     return [state, setState]
+  }
+
+  function useRef(initVal) {
+    let ref = {current: initVal};
+    return ref
+  }
+
+  function useEffect(cb, depArray) {
+    let olDeps = hooks[idx]
+    let hasChange = true;
+    
+    if(olDeps) {
+      hasChange = depArray.some((dep, i) => !Object.is(dep, olDeps[i]))
+    }
+
+    if(hasChange) cb()
+    hooks[idx] = depArray;
+    idx++;
   }
 
   function render(Component) {
@@ -20,15 +39,24 @@ const React = (function() {
 
   return {
     useState,
+    useRef,
     render,
+    useEffect,
   };
 })();
 
 function Component() {
   const [count, setCount] = React.useState(1);
   const [text, setText] = React.useState('apple');
+  const ref1 = React.useRef(2);
+  const ref2 = React.useRef(1);
+
+  React.useEffect(() => {
+    console.log('jsconfffff')
+  }, [count, text]);
+
   return {
-    render: () => console.log({count, text}),
+    render: () => console.log({count, text, ref1: ref1.current, ref2: ref2.current}),
     click: () => setCount(count + 1),
     type: () => setText('appear')
   }
@@ -37,11 +65,8 @@ function Component() {
 var App = React.render(Component)
 App.click()
 var App = React.render(Component)
-App.click()
-var App = React.render(Component)
-App.click()
-var App = React.render(Component)
-App.click()
-var App = React.render(Component)
 App.type()
 var App = React.render(Component)
+// var App = React.render(Component)
+// App.click()
+// App.click()
